a4fe9461dc0df587f753216fd3e4cfc181f3c6ea
Gafarov Stanislav <sgafarov@MacBook-Pro-localadmin.local>
2019-01-01 13:53:07 +0300

    Starting project. Added few TODO

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+if __name__ == "__main__":
+    print("Starting")
+    # TODO: getting Data from different tables and different db's
+    # TODO: cleaning phones and emails
+    # TODO: Saving DataFrames if it necessary
+    # TODO: Loading cleaning DataFrames into local db
+    # TODO: Adding multiprocessing
+    print("Done")
+
